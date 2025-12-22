c1e538d08a77c01bf7d4ac5cd2027fbfc7a66904
alessandracorreadev <bkpalecorrea@gmail.com>
2025-12-22 16:36:09 -0300

    ex07

---
+++ ex007-arithmetic-mean.py
@@ -0,0 +1,5 @@
+grade1 = float(input("Enter the student's first grade: "))
+grade2 = float(input("Enter the student's second grade: "))
+average = (grade1 + grade2) / 2
+
+print(f"The average of {grade1} and {grade2} is equal to {average:.1f}.")
