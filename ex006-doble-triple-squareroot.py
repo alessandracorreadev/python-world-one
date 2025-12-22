c004f850e7556aa7a1c247c0873c6189f8a1a858
alessandracorreadev <bkpalecorrea@gmail.com>
2025-12-22 15:56:51 -0300

    ex06

---
+++ ex006-doble-triple-squareroot.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+
+print(f"The doble of {num} is equal to {num*2}")
+print(f"The triple of {num} is equal to {num*3}")
+print(f"The square root of {num} is equal to {num**(1/2)}")
