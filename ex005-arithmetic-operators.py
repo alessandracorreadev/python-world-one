25a205d752c25250c9df282091c59d5f1d7b552a
alessandracorreadev <bkpalecorrea@gmail.com>
2025-12-22 15:25:03 -0300

    ex 05

---
+++ ex005-arithmetic-operators.py
@@ -0,0 +1,4 @@
+num = int(input("Enter a number: "))
+print(f"Analyzing the value {num}, its predecessor is {num-1} and its successor is {num+1}.")
+
+# Another option is to create variables to store the predecessor and the successor.
