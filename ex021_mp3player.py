34d0e5aaefcc19146a55411678dd9e086377a6a0
alessandracorreadev <bkpalecorrea@gmail.com>
2025-12-23 10:45:37 -0300

    ex 021

--- ex021_mp3player.py
+++ ex021_mp3player.py
@@ -1,5 +1,9 @@
 import pygame
+import time
 
 pygame.init() # intialize pygame
-pygame.mixer.music.load('only_me_audio_yt_lib.mp3') # load the mp3
-pygame.mixer.music.play() # play the mp3
+pygame.mixer.init()
+pygame.mixer.music.load("only_me_audio_yt_lib.mp3") # load the mp3
+pygame.mixer.music.play(-1) # play the mp3
+while pygame.mixer.music.get_busy(): # plays the mp3  in infinete loop
+    time.sleep(1)
