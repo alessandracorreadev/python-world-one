13ea0c262a4f87fb2b54757a24ddb4580cc84df0
alessandracorreadev <bkpalecorrea@gmail.com>
2025-12-23 06:41:51 -0300

    ex 13

---
+++ ex013_salary_adjustment.py
@@ -0,0 +1,4 @@
+salary = float(input("Enter the employee's salary: $"))
+new_salary = salary + (salary * 0.15)
+
+print(f"The employee who earned ${salary:.2f}, with a 15% raise, will now receive ${new_salary:.2f}.")
