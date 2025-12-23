100f5bb14947621af0a8ee3f71ccecdd757eb583
alessandracorreadev <bkpalecorrea@gmail.com>
2025-12-23 06:25:57 -0300

    ex 11

---
+++ ex011_painting_a_wall.py
@@ -0,0 +1,7 @@
+width = float(input("Wall width: "))
+heigth = float(input("Wall height: "))
+area = width * heigth
+paint = area / 2
+
+print(f"Your wall has dimensions of {width} x {heigth}, and it's area is {area}m².")
+print(f"To paint this wall, you will need {paint} liters of paint.")
