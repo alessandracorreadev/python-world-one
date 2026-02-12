eb814b5a2b5cdb046ec85df16fd787d344be25f2
alessandracorreadev <bkpalecorrea@gmail.com>
2026-02-12 10:08:08 -0300

    ex086 matrix

--- ex086_matrix_list.py
+++ ex086_matrix_list.py
@@ -1,12 +1,14 @@
-matrix = [[[], [], []],
-          [[], [], []],
-          [[], [], []]]
+matrix = [[], [], []]
 
 for row in range(0, 3):
     for col in range(0, 3):
         number = int(input(f"Enter the value for [{row}, {col}]: "))
-        matrix[row][col].append(number)
+        matrix[row].append(number)
 
 for c in range(0, 3):
     print(f"{matrix[c]}")
 
+for r in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matrix[r][c]:^5}]', end='')
+    print()
