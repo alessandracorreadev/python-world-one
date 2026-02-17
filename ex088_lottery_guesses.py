a11749a1c0503e674cc4483720a8a15004b2d0a4
alessandracorreadev <bkpalecorrea@gmail.com>
2026-02-17 13:26:16 -0300

    ex088 the lottery game

--- ex088_lottery_guesses.py
+++ ex088_lottery_guesses.py
@@ -1,11 +1,16 @@
 from random import randint
 from random import sample
+from time import sleep
 
 all_sets = []
-temporary_set = []
 
 guesses = int(input("Enter the number of sets you want: "))
 
 for sets in range(0, guesses):
-    for guess in range(0, 6):
-        temporary_set.append(sample(range(1, 61), 6))
+    all_sets.append(sorted(sample(range(0, 61), 6)))
+
+print('-=' * 5, f"GENERATING {guesses} LOTTERY TICKETS", '=-' * 5)
+for ticket, each_set in enumerate(all_sets):
+    print(f"Ticket {ticket+1}: {each_set}")
+    sleep(1)
+print('-=' * 9, "GOOD LUCK!", '=-' * 9)
---
+++ ex999_for_tests.py
@@ -0,0 +1,5 @@
+jogos = [[10, 20], [5, 50], [30, 1]]
+
+sum_order = sorted(jogos, key=sum)
+
+print(sum_order)
