f9978c9c13e1c61aba65db3139010840259ffc5f
alessandracorreadev <bkpalecorrea@gmail.com>
2026-06-25 10:14:15 -0300

    fix: correct superclass and subclass methods and add tests from remaining classes.

--- pythonPOO/exercises/ex024_poo_coffee_machine/__main__.py
+++ pythonPOO/exercises/ex024_poo_coffee_machine/__main__.py
@@ -2,7 +2,13 @@ from ex024_poo_coffee_machine import *
 
 def main():
     e1 = Espresso()
-    e1.mix()
+    e1.prepare()
+
+    t1 = Tea()
+    t1.prepare()
+
+    l1 = Latte()
+    l1.prepare()
 
 if __name__ == "__main__":
     main()
--- pythonPOO/exercises/ex024_poo_coffee_machine/ex024_poo_coffee_machine.py
+++ pythonPOO/exercises/ex024_poo_coffee_machine/ex024_poo_coffee_machine.py
@@ -1,12 +1,15 @@
 from abc import ABC, abstractmethod
 
-
 class HotBeverage(ABC):
     def prepare(self):
-        pass
+        print("-" * 20 + "Starting preparation" + "-" * 20)
+        self.boil_water()
+        self.mix()
+        self.serve()
+        print("-" * 20 + "Drink Ready" + "-" * 20)
 
     def boil_water(self):
-        pass
+        print("1. Boiling water to 100 degrees Celsius.")
 
     @abstractmethod
     def mix(self):
@@ -19,34 +22,22 @@ class HotBeverage(ABC):
 
 class Espresso(HotBeverage):
     def mix(self):
-        print("-" * 10 + "Starting preparation" + "-" * 10)
-        print(f'''1. Boiling water to 100 degrees Celsius.
-2. Passing pressurized water through the ground coffee.''')
-        self.serve()
+        print("2. Passing pressurized water through the ground coffee.")
 
     def serve(self):
         print("3. Serving in a small cup.")
-        print("-" * 10 + "Drink Ready" + "-" * 10)
 
 class Tea(HotBeverage):
     def mix(self):
-        print("-" * 10 + "Starting preparation" + "-" * 10)
-        print('''1. Boiling water to 100 degrees Celsius.
-2. Steeping a herbal tea bag in the water.''')
-        self.serve()
+        print("2. Steeping a herbal tea bag in the water.")
 
     def serve(self):
         print("3. Serving in a porcelain mug with lemon.")
-        print("-" * 10 + "Drink Ready" + "-" * 10)
 
 class Latte(HotBeverage):
     def mix(self):
-        print("-" * 10 + "Starting preparation" + "-" * 10)
-        print('''1. Boiling water to 100 degrees Celsius.
-2. Passing pressurized steam through the milk.''')
-        self.serve()
+        print("2. Passing pressurized steam through the milk.")
 
     def serve(self):
         print("3. Serving in a large mug.")
-        print("-"*10 + "Drink Ready" + "-"*10)
 
